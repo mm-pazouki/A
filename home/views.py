7468d1f87589c91e95dcc643220eca8b8d0251f5
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-04 19:30:37 +0330

    forms 2

--- home/views.py
+++ home/views.py
@@ -21,8 +21,16 @@ def delete(request, todo_id):
 
 
 def create(request):
-    form = TodoCreateForm()
-    return render(request, 'create.html', {'form':form})
+    if request.method == 'POST':
+        form = TodoCreateForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            Todo.objects.create(title=cd['title'], body=cd['body'], created=cd['created'])
+            messages.success(request, 'Todo created successfully', 'success')
+            return redirect('root')
+    else:
+        form = TodoCreateForm()
+    return render(request, 'create.html', {'form': form})
 
 
 
