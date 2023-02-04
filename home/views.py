0ed72260a4eb135b5129d8b5bc20d681162cb2d1
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-04 09:33:22 +0330

    work with models

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
+
 
 def home(request):
-    return  render(request, 'root.html')
+    all = Todo.objects.all()
+    return render(request, 'root.html', {'todos': all})
+
 
 def say_hello(request):
     person = {'name': 'mehdi'}
