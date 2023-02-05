bd0479233b3c25bf32338a32410559e95abf46eb
Mr Pazouki <mm_pazouki@yahoo.com>
2023-02-05 11:06:14 +0330

    Permission set

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,14 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.user_register, name='user_register'),
-]
-
-urlpatterns = [
+    path('register/', views.user_register, name='register'),
     path('login/', views.user_login, name='login'),
-]
-
-
-urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
 ]
+
